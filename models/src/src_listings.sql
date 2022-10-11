1ff7c2d34dc752de13cc40b391d6388c710d7da7
Phoebe Chen <phoebehch@gmail.com>
2022-10-10 20:44:22 -0400

    Hotfix src_listings

--- models/src/src_listings.sql
+++ models/src/src_listings.sql
@@ -5,11 +5,12 @@ with
     )
 
 select
-    id                 as listing_id,
+    id                 as listing_id
     , name             as listing_name
     , listing_url      as listing_url
     , room_type        as room_type
     , minimum_nights   as minimum_nights
+    , host_id          as host_id
     , price            as price_str
     , created_at       as created_at
     , updated_at       as updated_at
