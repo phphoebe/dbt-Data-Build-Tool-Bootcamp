55472559397b00ad7cebf7676bae5a1b9eb44500
Phoebe Chen <phoebehch@gmail.com>
2022-10-26 18:36:01 -0400

    Create Incremental Model: fact_reviews

---
+++ models/fct/fact_reviews.sql
@@ -0,0 +1,22 @@
+{{
+    config (
+        materialized = 'incremental',
+        on_schema_change = 'fail'
+    )
+}}
+
+with 
+    src_reviews as (
+        select *
+        from {{ ref('src_reviews')}}
+    )
+
+select * from src_reviews
+where review_text is not null 
+
+/* Tell dbt how to increment the new records */
+{% if is_incremental() %}
+    and review_date > (select max(review_date) from {{ this }})
+{% endif %}
+
+
